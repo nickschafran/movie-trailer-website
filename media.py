f733a04b745d4377e24a1a804e5413a032728dc2
Nick Schafran <nickschafran@users.noreply.github.com>
2015-06-25 16:39:08 -0400

    Update media.py

--- media.py
+++ media.py
@@ -1,9 +1,12 @@
 import webbrowser
+# Define Movie Class
 
-#Define Movie Class
+
 class Movie():
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		# self.title makes title an instance variable
+		"""
+		self.title makes title an instance variable
+		"""
 		self.title =  movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
